81f5bff577736f309d5f0bbf6bdf2162b9769999
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2023-09-08 20:09:07 +0300

    Add project files.

---
+++ HomeWork03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace HomeWork03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("✨ This is Hillel 2023 C# Pro homework #03");
+        }
+    }
+}
